11e9cedde6463b7054d49138abc13563d655bd85
PaBochka <bochenkovp@yandex.ru>
2018-09-16 00:51:58 +0300

    v.2

--- math_part.py
+++ math_part.py
@@ -10,7 +10,8 @@ x0 = float(input ("begin x:"))
 R = float(input ("begin R:"))
 w = float(input ("begin w:"))
 E = float(input ("begin E:"))
-exp = 0.01
+exp = 0.00001
+points = []
 def f(x, I):
     return (E*(math.sin(w * x)) - R * I)/L
 def loc_err(step_I, two_step_I):
@@ -27,6 +28,59 @@ def next_point_I(I, x, step):
     return I + (step_func1(step, x, I) + 4*step_func2(step, x, I) + step_func3(step, x, I))/6
 
 ##################################################
+def new_point(step, x, I):
+
+    global new_I
+    new_I = I
+    global new_x
+    new_x = x
+    new_I = next_point_I(new_I, new_x, step)
+    new_x = next_point_x(step, new_x)
+
+    global tmp_I
+    tmp_I = new_I
+    global tmp_x
+    tmp_x = new_x
+    global tmp_h
+    tmp_h = step
+
+    print ("func next I:", new_I)
+    print ("func next x:", new_x)
+    
+    global add_I
+    add_I = I
+    global add_x
+    add_x = x
+    add_I = next_point_I(add_I, add_x, step/2)
+    add_x = next_point_x(step/2, add_x)
+
+    print ("func add I:", add_I)
+    print ("func add x:", add_x)
+
+    new_add_I = next_point_I(add_I, add_x, step/2)
+    new_add_x = next_point_x(step/2, add_x)
+
+    print ("func next I:", new_add_I)
+    print ("func next x:", new_add_x)
+
+    S = loc_err(new_I, new_add_I)
+
+    print("S####: ", S)
+    print("exp###: ", exp/16, exp)
+
+    global new_h
+    if abs(S) >= exp/16 and abs(S) <= exp:
+        print("save point")
+        new_h = h
+        return (new_I, new_x)
+    if abs(S) < exp/16:
+        print("save point, but change step")
+        new_h = 2*h
+        return (new_I, new_x)
+    if abs(S) > exp:
+        print("Fail")
+        curr_h = tmp_h/2
+        return new_point(curr_h, tmp_x, tmp_I)
 
 new_x = x0
 new_I = I0
@@ -36,46 +90,10 @@ new_h = h
 tmp_I = 0.0
 tmp_x = 0.0
 tmp_h = h
-for i in range(1, n):
-
-    new_I = next_point_I(new_I, new_x, new_h)
-    new_x = next_point_x(new_h, new_x)
-
-    tmp_I = new_I
-    tmp_x = new_x
-    tmp_h = new_h
-
-    print ("func next I:", new_I)
-    print ("func next x:", new_x)
+for i in range(n):
     
-    add_I = next_point_I(add_I, add_x, new_h/2)
-    add_x = next_point_x(new_h/2, add_x)
+    points.append(new_point(new_h, new_x, new_I))
 
-    print ("func add I:", add_I)
-    print ("func add x:", add_x)
+for i in points:
 
-    new_add_I = next_point_I(add_I, add_x, new_h/2)
-    new_add_x = next_point_x(new_h/2, add_x)
-
-    print ("func next I:", new_add_I)
-    print ("func next x:", new_add_x)
-
-    S = loc_err(new_I, new_add_I)
-
-    if abs(S) >= exp/16 and abs(S) <= exp:
-        print("save point")
-        new_h = h
-    if abs(S) < exp/16:
-        print("save point, but change step")
-        new_h = 2*h
-    if abs(S) > exp:
-        print("Fail")
-        curr_h = tmp_h/2
-        new_I = next_point_I(tmp_I, tmp_x, curr_h)
-        new_x = next_point_x(curr_h,tmp_x)
-        tmp_I = new_I
-        tmp_x = new_x
-        tmp_h = curr_h
-
-
-
+    print(i, '\n')
